a5bb77e522479a763202339a578ccf05c624cac0
extiser <1sappho@gmail.com>
2017-08-02 12:13:34 +0500

    admin pages

--- builds/static/js/main.js
+++ builds/static/js/main.js
@@ -2240,6 +2240,24 @@
 
 
 
+$('.jsFormRedactChange').click(function (e) {
+  e.preventDefault();
+  var $this = $(this),
+      currentValue = $this.closest('.jsFormRedact').find('.jsFormRedactView').html();
+
+  $this.closest('.jsFormRedact').find('.jsFormRedactControl, .jsFormRedactView').addClass('_hidden');
+  $this.closest('.jsFormRedact').find('.jsFormRedactHidden').addClass('_visible');
+  $this.closest('.jsFormRedact').find('.jsFormRedactField').val(currentValue).focus();
+});
+
+$('.jsFormRedactCancel').click(function (e) {
+  e.preventDefault();
+  var $this = $(this);
+
+  $this.closest('.jsFormRedact').find('.jsFormRedactControl, .jsFormRedactView').removeClass('_hidden');
+  $this.closest('.jsFormRedact').find('.jsFormRedactHidden').removeClass('_visible');
+});
+
 
 
 $('[data-modal]').click(function (e) {
